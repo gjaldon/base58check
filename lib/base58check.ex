ad4275d08b2661b0931ac1591c56763372e62b41
Gabriel Jaldon <gjaldon85@gmail.com>
2015-01-21 18:37:58 +0800

    Decode58/2 verifies checksum and returns decode in binary

--- lib/base58check.ex
+++ lib/base58check.ex
@@ -16,14 +16,14 @@ defmodule Base58Check do
 		encode58(div(integer, 58), [do_encode58(rem(integer, 58)) | acc])
 	end	
 
-	def decode58(data) when is_binary(data) do
-		decode58(to_char_list(data), 0)
+	def decode58(code) when is_binary(code) do
+		decode58(to_char_list(code), 0)
 	end
+	def decode58(code), do: raise(ArgumentError, "expects base58-encoded binary")
 	defp decode58([], acc), do: acc
-	defp decode58([c|codestring], acc) do
-		decode58(codestring, (acc * 58) + do_decode58(c))
+	defp decode58([c|code], acc) do
+		decode58(code, (acc * 58) + do_decode58(c))
 	end
-	def decode58(data), do: raise(ArgumentError, "expects base58-encoded binary")
 
 	def encode58check(prefix, data) when is_binary(prefix) and is_binary(data) do
 		data = case Base.decode16(String.upcase(data)) do
@@ -31,12 +31,29 @@ defmodule Base58Check do
 				:error 			->	data
 			end
 		versioned_data = prefix <> data
-		<<checksum::binary-size(4), _rest::binary-size(28)>> = versioned_data |> :erlsha2.sha256() |> :erlsha2.sha256()
-		encode58((versioned_data <> checksum))
+		checksum = generate_checksum(versioned_data)
+		encode58(versioned_data <> checksum)
 	end
 	def encode58check(prefix, data) do
 		prefix = if is_integer(prefix), do: :binary.encode_unsigned(prefix), else: prefix
 		data = if is_integer(data), do: :binary.encode_unsigned(data), else: data
 		encode58check(prefix, data)
+	end
+
+	def decode58check(code) when is_binary(code) do
+		decoded_bin = decode58(code) |> :binary.encode_unsigned()
+		payload_size = byte_size(decoded_bin) - 5
+
+		<<prefix::binary-size(1), payload::binary-size(payload_size), checksum::binary-size(4)>> = decoded_bin
+		if generate_checksum(prefix <> payload) == checksum do
+			payload
+		else
+			raise ArgumentError, "checksum doesn't match"
+		end
+	end
+
+	defp generate_checksum(versioned_data) do
+		<<checksum::binary-size(4), _rest::binary-size(28)>> = versioned_data |> :erlsha2.sha256() |> :erlsha2.sha256()
+		checksum
 	end
 end
--- test/base58check_test.exs
+++ test/base58check_test.exs
@@ -21,20 +21,32 @@ defmodule Base58CheckTest do
     end
   end
 
+  @test_hex "1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd"
+  @test_base58 "5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jcn"
+
   test "encode58check/2 accepts integer" do
-  	bin = Base.decode16! "1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd", case: :lower
+  	bin = Base.decode16! @test_hex, case: :lower
   	integer = :binary.decode_unsigned(bin)
-    assert encode58check(128, integer) == "5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jcn"
+    assert encode58check(128, integer) == @test_base58
   end
 
   test "encode58check/2 accepts binary" do
-  	data_bin = Base.decode16! "1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd", case: :lower
+  	data_bin = Base.decode16! @test_hex, case: :lower
   	prefix_bin = :binary.encode_unsigned(128)
-    assert encode58check(prefix_bin, data_bin) == "5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jcn"
+    assert encode58check(prefix_bin, data_bin) == @test_base58
   end
 
   test "encode58check/2 accepts hex" do
-  	bin = "1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd"
-    assert encode58check(128, bin) == "5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jcn"
+    assert encode58check(128, @test_hex) == @test_base58
+  end
+
+  test "decode58check/2 accepts hex and returns binary" do
+    assert decode58check(@test_base58) |> Base.encode16(case: :lower) == @test_hex
+  end
+
+  test "decode58check/2 raises when checksum doesn't match" do
+    assert_raise ArgumentError, fn -> 
+    	decode58check("5J3mBbAH58CpQ3Y5RNJpUKPE62SQ5tfcvU2JpbnkeyhfsYB1Jc")
+    end
   end
 end
