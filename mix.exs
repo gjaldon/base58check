b3f0dabf85d995a5fada3aff522511170f5e0e4e
Esdras Eduardo <esdrasedu@gmail.com>
2017-12-15 17:19:19 -0200

    using :crypto for hash sha256

--- mix.exs
+++ mix.exs
@@ -3,10 +3,10 @@ defmodule Base58Check.Mixfile do
 
   def project do
     [app: :base58check,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.0",
-     deps: deps,
-     package: package,
+     deps: deps(),
+     package: package(),
      name: "Base58Check",
      source_url: "https://github.com/gjaldon/base58check",
      homepage_url: "https://github.com/gjaldon/base58check",
@@ -25,7 +25,6 @@ defmodule Base58Check.Mixfile do
      links: %{github: "https://github.com/gjaldon/base58check"}]
   end
 
-  defp deps do
-    [{:erlsha2, github: "vinoski/erlsha2"}]
-  end
+  defp deps, do: []
+
 end
